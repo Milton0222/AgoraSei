4b78ee670f3e24bda8970c72717f60b83131b2b4
Milton0222 <francimilton1997@gmail.com>
2026-05-27 22:46:33 +0100

    seeder admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::create([
+             'name' => 'Test User',
+             'email' => 'kizalu@gmail.com',
+             'password'=>bcrypt(123456789),
+             'isAdmin'=>1,
+             'isEst'=>1
+         ]);
     }
 }
